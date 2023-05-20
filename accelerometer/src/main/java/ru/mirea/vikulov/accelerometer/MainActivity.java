9e384935e9f851ed30024bb1a2f783b00686c28b
putk4k <putk4k@mail.ru>
2023-05-20 19:50:55 +0300

    Initial commit

---
+++ accelerometer/src/main/java/ru/mirea/vikulov/accelerometer/MainActivity.java
@@ -0,0 +1,56 @@
+package ru.mirea.vikulov.accelerometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import ru.mirea.vikulov.accelerometer.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ActivityMainBinding binding;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometerSensor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometerSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float valueAzimuth = event.values[0];
+            float valuePitch = event.values[1];
+            float valueRoll = event.values[2];
+            binding.textViewAzimuth.setText("Azimuth: " + valueAzimuth);
+            binding.textViewPitch.setText("Pitch: " + valuePitch);
+            binding.textViewRoll.setText("Roll: " + valueRoll);
+        }
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}
